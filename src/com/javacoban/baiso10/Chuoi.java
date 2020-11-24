b3a88150d9d699d04bd41717bdf19f8374bdb2b8
MatalusMaster <phuocsang.cse@gmail.com>
2020-11-24 21:58:57 +0700

    Basic String in Java

---
+++ src/com/javacoban/baiso10/Chuoi.java
@@ -0,0 +1,19 @@
+package com.javacoban.baiso10;
+
+public class Chuoi {
+
+	public static void main(String[] args) {
+		String x = "Toi ten la";
+		
+		System.out.println(x);
+		
+		String t = "Ho Phuoc Sang";
+		String ghep = x + t;
+		System.out.println(ghep);
+		
+		int i = 10;
+		System.out.println(t + i);
+		System.out.println("Gia tri cua i = " + i);
+		
+	}
+}
