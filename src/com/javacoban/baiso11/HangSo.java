f0b59b2b26fe67df97f3fb6db0a17e8d46231ef5
MatalusMaster <phuocsang.cse@gmail.com>
2020-11-24 22:03:21 +0700

    Constant in Java

---
+++ src/com/javacoban/baiso11/HangSo.java
@@ -0,0 +1,19 @@
+package com.javacoban.baiso11;
+
+public class HangSo {
+
+	public static final int HANG_SO = 10;
+	public static final float SO_PI = 3.14f;
+	public static final String TEN_TUI_LA = "Ho Phuoc Sang";
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Xin Chao" + TEN_TUI_LA);
+		
+		float chuVi = SO_PI * 2;
+		
+		System.out.println("Chu vi duong tron = " + chuVi);
+		
+	}
+
+}
