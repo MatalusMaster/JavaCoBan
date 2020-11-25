8130fcebeb018c616ecf36c29582d2c1a51afed8
MatalusMaster <phuocsang.cse@gmail.com>
2020-11-25 20:57:14 +0700

    Array in java

---
+++ src/com/javacoban/bai20/Mang.java
@@ -0,0 +1,19 @@
+package com.javacoban.bai20;
+
+public class Mang {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] mangInt = new int[3];
+		mangInt[0] = 0;
+		mangInt[1] = 1;
+		mangInt[2] = 2;
+		
+		System.out.println( mangInt[1] + mangInt[2]);
+		
+		long[] arr = {1,2,3,4,5};
+		System.out.println("Chieu dai mang arr : " + arr.length);
+		
+	}
+
+}
