d3c6fab25bd2882ebc031110a37117acf38d17e0
MatalusMaster <phuocsang.cse@gmail.com>
2020-11-25 21:26:42 +0700

    For Each loop

---
+++ src/com/javacoban/bai24/VongLapForEach.java
@@ -0,0 +1,18 @@
+package com.javacoban.bai24;
+
+public class VongLapForEach {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+		for (int i : arr) {
+			System.out.println(i);
+		}
+
+		String[] str = { "Tui", "Ten", "La", "Sang" };
+		for (String s : str) {
+			System.out.println(s);
+		}
+	}
+
+}
