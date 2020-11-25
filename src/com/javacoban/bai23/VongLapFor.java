ee01916e2f688938bf756efbd263cf6cac1e7068
MatalusMaster <phuocsang.cse@gmail.com>
2020-11-25 21:17:26 +0700

    For loop

---
+++ src/com/javacoban/bai23/VongLapFor.java
@@ -0,0 +1,17 @@
+package com.javacoban.bai23;
+
+public class VongLapFor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < 5; i ++) {
+			System.out.println("i = " + i);
+		}
+		
+		int[] arr = {1,2,3,4,5,6};
+		for(int index = 0; index < arr.length; index++) {
+			System.out.println("arr[" + index + "] = " + arr[index]);
+		}
+	}	
+
+}
