3eb46f9bce2f297d7b6f97803b7fa5b1250d9b66
MatalusMaster <phuocsang.cse@gmail.com>
2020-11-26 23:50:25 +0700

    phan biet null va 0

---
+++ src/com/javacoban/bai39/Main.java
@@ -0,0 +1,17 @@
+package com.javacoban.bai39;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 0;
+		float k = 0.0f;
+
+		Person p = null;
+		// p.setTen("Sang"); => se bao loi vi p dang la null
+
+		p = new Person();
+		p.setTen("Sang");
+	}
+
+}
---
+++ src/com/javacoban/bai39/Person.java
@@ -0,0 +1,14 @@
+package com.javacoban.bai39;
+
+public class Person {
+	private String ten;
+
+	public String getTen() {
+		return ten;
+	}
+
+	public void setTen(String ten) {
+		this.ten = ten;
+	}
+
+}
