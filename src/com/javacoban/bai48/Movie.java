cccb63343fcc411db690c281872671b3a54ee2f7
MatalusMaster <phuocsang.cse@gmail.com>
2020-12-21 23:30:00 +0700

    class object

---
+++ src/com/javacoban/bai48/Movie.java
@@ -0,0 +1,10 @@
+package com.javacoban.bai48;
+
+public class Movie {
+	public static void main(String[] args) {
+		Movie mv = new Movie();
+		
+		Object ob = new Object(); // class object la class cha cua moi class
+		
+	}
+}
