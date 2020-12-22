a3855b8f6e5c2428502eff3a5558539b4beaa6b0
MatalusMaster <phuocsang.cse@gmail.com>
2020-12-22 22:09:56 +0700

    interface in java

---
+++ src/com/javacoban/bai54/AnimalServiceImp.java
@@ -0,0 +1,23 @@
+package com.javacoban.bai54;
+
+public class AnimalServiceImp implements AnimalService {
+
+	@Override
+	public void them() {
+		// TODO Auto-generated method stub
+		System.out.println("them");
+	}
+
+	@Override
+	public void sua() {
+		// TODO Auto-generated method stub
+		System.out.println("sua");
+	}
+
+	@Override
+	public void xoa() {
+		// TODO Auto-generated method stub
+		System.out.println("xoa");
+	}
+
+}
---
+++ src/com/javacoban/bai54/Main.java
@@ -0,0 +1,12 @@
+package com.javacoban.bai54;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AnimalServiceImp ani = new AnimalServiceImp();
+
+		AnimalService animalService = new AnimalServiceImp(); // Tinh da hinh
+	}
+
+}
